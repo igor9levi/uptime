69cddbed6214cf5a87df0360722c6520c062e23e
Igor Radovic <radovici@ae.com>
2020-04-05 20:54:55 +0200

    Fix indentation

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,8 +3,8 @@ import ENV from 'uptime/config/environment';
 
 export default class IndexRoute extends Route {
     async model() {
-        const response = await fetch(ENV.CHECKS_API);
-        const parsed = await response.json();
-        return parsed;
-      }
+      const response = await fetch(ENV.CHECKS_API);
+      const parsed = await response.json();
+      return parsed;
+    }
 }
